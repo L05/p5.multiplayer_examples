73062b23f1d656d5a6643c2106d57d56b0ea39b4
L05 <carlosga@gmail.com>
2019-09-11 22:02:20 -0700

    Initialized p5multiplayer_host.tox. Socket IO DAT communication working with server.

---
+++ p5_mp_TouchDesigner/Modules/HostExt.py
@@ -0,0 +1,59 @@
+"""
+Extension classes enhance TouchDesigner components with python. An
+extension is accessed via ext.ExtensionClassName from any operator
+within the extended component. If the extension is promoted via its
+Promote Extension parameter, all its attributes with capitalized names
+can be accessed externally, e.g. op('yourComp').PromotedFunction().
+
+Help: search "Extensions" in wiki
+"""
+
+from TDStoreTools import StorageManager
+TDF = op.TDModules.mod.TDFunctions
+
+class HostExt:
+	"""
+	HostExt description
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+		self.roomId = str(self.ownerComp.par.Roomid)
+		self.id = None
+		self.socketIoDat = op('iparHost').par.Socketiodat
+
+		self.hostConnected = False
+		
+		return
+
+	def OnId(self, data):
+		self.id = data
+
+		print("id: " + self.id)
+		return
+
+	def Connect(self):
+		data = { 'name': 'host', 'roomId': self.roomId }
+		self.socketIoDat.eval().emit('join', data=data)
+
+		return
+
+	def OnHostConnect(self, data):
+		print("Host connected to server.")
+		self.hostConnected = True
+		
+		if (not self.roomId):
+			self.roomId = data.roomId
+		return
+
+	def OnClientConnect(self, data):
+		print("Client connected to server.")
+		return
+	
+	def OnClientDisconnect(self, data):
+		print("Client disconnected to server.")
+		return
+	
+	def OnReceiveData(self, data):
+		print(data)
+		return
